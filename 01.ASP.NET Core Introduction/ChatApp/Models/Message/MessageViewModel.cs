06b8a9eab846ad53ef22e272d55f00cf70476ff8
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-02-02 15:14:23 +0200

    Updated ChatApp! Added some validations!

--- 01.ASP.NET Core Introduction/ChatApp/Models/Message/MessageViewModel.cs
+++ 01.ASP.NET Core Introduction/ChatApp/Models/Message/MessageViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace ChatApp.Models.Message
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models.Message
 {
     public class MessageViewModel
     {
+        [Display(Name = "Sender Name")]
+        [Required(ErrorMessage = "Field {0} is required!")]
         public string Sender { get; set; } = null!;
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Field {0} is required!")]
         public string MessageText { get; set; } = null!;
     }
 }
