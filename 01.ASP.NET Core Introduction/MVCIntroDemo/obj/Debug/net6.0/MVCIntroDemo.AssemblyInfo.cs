5ce910066330f53bd8b8c34f62bb3a563503f02b
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-02-02 14:43:47 +0200

    Commited some changes!

--- 01.ASP.NET Core Introduction/MVCIntroDemo/obj/Debug/net6.0/MVCIntroDemo.AssemblyInfo.cs
+++ 01.ASP.NET Core Introduction/MVCIntroDemo/obj/Debug/net6.0/MVCIntroDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCIntroDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d17727828230641fc31afc80bfcd0351d37b77")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCIntroDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCIntroDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
