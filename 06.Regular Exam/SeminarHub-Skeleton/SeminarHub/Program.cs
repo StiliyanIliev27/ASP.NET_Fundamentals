7dd4ac86199c0adaf199bd1607c242f56343a48d
Stiliyan_Iliev <142061336+StiliyanIliev27@users.noreply.github.com>
2024-05-06 23:51:49 +0300

    Update Program.cs

--- 06.Regular Exam/SeminarHub-Skeleton/SeminarHub/Program.cs
+++ 06.Regular Exam/SeminarHub-Skeleton/SeminarHub/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
-using SeminarHub.Data;
+using SeminarHub.Data; 
 
 namespace SeminarHub
 {
